7011a06690e25d58ffbbe216a1fc5253de7c5ec6
StevenBuglione <stevenbuglione1@gmail.com>
2025-09-23 21:03:31 -0400

    Updating Configuration

--- infra/flake.nix
+++ infra/flake.nix
@@ -29,7 +29,7 @@
         ./modules/common/laptop-as-server.nix 
         ./modules/common/networking.nix 
         ./modules/common/updates.nix
-        ./modules/common/docker-hummingbot.nix
+        ./modules/common/docker.nix
       ];  
 
       dellModules = commonModules ++ [
